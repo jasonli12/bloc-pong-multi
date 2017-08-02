14dcf25264562d58b48d37bf373b6ec3c5aad7ac
Jason Li <jason.li0102@gmail.com>
2017-08-01 22:52:46 -0700

    Add speed change and refactor ball prototype render method

--- scripts/app.js
+++ scripts/app.js
@@ -50,12 +50,15 @@ function Computer(x, y, width, height, speed) {
   this.speed = speed;
 }
 
-function Ball(x, y, radius, speed, direction = Math.random() * Math.PI * 2) {
+function Ball(x, y, radius, speed, direction = Math.random() * Math.PI * 2, minSpeed = 10, maxSpeed = 30, speedChange = 2) {
   this.x = x;
   this.y = y;
   this.radius = radius;
   this.speed = speed;
   this.direction = direction;
+  this.minSpeed = minSpeed;
+  this.maxSpeed = maxSpeed;
+  this.speedChange = speedChange
 }
 
 Player.prototype = {
@@ -93,39 +96,86 @@ Computer.prototype.render = function() {
   fieldContext.fillRect(this.x, this.y, this.width, this.height);
 };
 
-Ball.prototype.render = function() {
-  // console.log(this.direction);
-  var fieldContext = field.fieldContext;
-  fieldContext.beginPath();
-  fieldContext.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-  fieldContext.fillStyle = "#72ff00";
-  fieldContext.fill();
-  this.x -= this.speed * Math.cos(this.direction);
-  this.y += this.speed * Math.sin(this.direction);
+Ball.prototype = {
+  render: function() {
+    var fieldContext = field.fieldContext;
+    fieldContext.beginPath();
+    fieldContext.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    fieldContext.fillStyle = "#72ff00";
+    fieldContext.fill();
+    this.x -= this.speed * Math.cos(this.direction);
+    this.y += this.speed * Math.sin(this.direction);
+    this.updateBallQuadrant();
 
-  var hitPaddle;
-  // Make ball bounce off walls
-  if (this.x >= fieldCanvas.width && (this.direction > Math.PI / 2 && this.direction <= Math.PI))  {
-    this.x = fieldCanvas.width
-    this.direction = Math.PI - this.direction;
-  } else if (this.x >= fieldCanvas.width && (this.direction >= Math.PI && this.direction < 3 * Math.PI / 2))  {
-    this.direction = 2 * Math.PI - (this.direction - Math.PI);
-  } else if (this.x <= 0 && (this.direction >= 0 && this.direction < Math.PI / 2)) {
-    this.x = 0;
-    this.direction = Math.PI - this.direction;
-  } else if (this.x <= 0 && (this.direction > 3 * Math.PI / 2 && this.direction <= 2* Math.PI)) {
-    this.x = 0;
-    this.direction = Math.PI + (2 * Math.PI - this.direction);
-  } else if ((this.x + this.radius >= playerOne.x) && (this.x - this.radius <= playerOne.x + playerOne.width) && (this.y + this.radius >= playerOne.y) && (this.y - this.radius < playerOne.y + playerOne.height) && (this.direction >= 0 && this.direction <= Math.PI / 2)) {
-    this.direction = 2 * Math.PI - (Math.PI - this.direction);
-    hitPaddle = true;
-  } else if ((this.x + this.radius >= playerOne.x) && (this.x - this.radius <= playerOne.x + playerOne.width) && (this.y + this.radius >= playerOne.y) && (this.y - this.radius < playerOne.y + playerOne.height) && (this.direction >= Math.PI / 2 && this.direction <= Math.PI)) {
-    this.direction = Math.PI + (Math.PI - this.direction);
-    hitPaddle = true;
-  } else if (this.y <= 0 && (this.direction >= Math.PI && this.direction <= 3 * Math.PI / 2)) {
+    // Make ball bounce off walls
+    var wallContact = this.x >= fieldCanvas.width || this.x <= 0;
+    var paddleContact = (this.x + this.radius >= playerOne.x) && (this.x - this.radius <= playerOne.x + playerOne.width) && (this.y + this.radius >= playerOne.y) && (this.y - this.radius < playerOne.y + playerOne.height - 10);
+    if (wallContact) {
+      if (this.ballQuadrant == 'Q2') {
+        this.x = fieldCanvas.width
+        this.direction = Math.PI - this.direction;
+      } else if (this.ballQuadrant == 'Q3')  {
+        this.direction = 2 * Math.PI - (this.direction - Math.PI);
+      } else if (this.ballQuadrant == 'Q1') {
+        this.x = 0;
+        this.direction = Math.PI - this.direction;
+      } else if (this.ballQuadrant == 'Q4') {
+        this.x = 0;
+        this.direction = Math.PI + (2 * Math.PI - this.direction);
+    }
+
+
+    // Make ball bounce off paddles
+  } else if (paddleContact) {
+    if (this.ballQuadrant == 'Q1'){
+      // speed change if holding down on direction key
+      if (keyDirection == 'right' && isPressed) {
+        if (this.speed - this.speedChange <= this.minSpeed) {
+          this.speed = this.minSpeed;
+        } else {
+          this.speed -= this.speedChange;
+        }
+      } else if (keyDirection == 'left' && isPressed) {
+        if (this.speed + this.speedChange >= this.maxSpeed) {
+          this.speed = this.maxSpeed;
+        } else {
+          this.speed += this.speedChange;
+        }
+      }
+      this.direction = 2 * Math.PI - this.direction;
+    } else if (this.ballQuadrant == 'Q2') {
+      // speed change if holding down on direction key
+      if (keyDirection == 'left' && isPressed) {
+        if (this.speed - this.speedChange <= this.minSpeed) {
+          this.speed = this.minSpeed;
+        } else {
+          this.speed -= this.speedChange;
+        }
+      } else if (keyDirection == 'right' && isPressed) {
+        if (this.speed + this.speedChange >= this.maxSpeed) {
+          this.speed = this.maxSpeed;
+        } else {
+          this.speed += this.speedChange;
+        }
+      }
+      this.direction = Math.PI + (Math.PI - this.direction);
+    }
+  } else if (this.y <= 0 && (this.ballQuadrant == 'Q3')) {
     this.direction = Math.PI - (this.direction - Math.PI);
-  } else if (this.y <= 0 && (this.direction >= 3 * Math.PI / 2 && this.direction <= 2* Math.PI)) {
+  } else if (this.y <= 0 && this.ballQuadrant == 'Q4') {
     this.direction = 2 * Math.PI - this.direction;
+  }
+  },
+  updateBallQuadrant: function() {
+    if (this.direction >= 0 && this.direction <= Math.PI / 2) {
+      this.ballQuadrant = 'Q1'
+    } else if (this.direction >= Math.PI / 2 && this.direction <= Math.PI) {
+      this.ballQuadrant = 'Q2'
+    } else if (this.direction >= Math.PI && this.direction <= 3 * Math.PI / 2) {
+      this.ballQuadrant = 'Q3'
+    } else {
+      this.ballQuadrant = 'Q4'
+    }
   }
 };
 
@@ -133,6 +183,7 @@ var field = new Field(fieldCanvas);
 var playerOne = new Player(260, 770, 80, 15, 10);
 var computerOne = new Computer(260, 10, 80, 15, 50);
 var ball = new Ball(300, 400, 12, 10);
+
 
 var animate = window.requestAnimationFrame || function(step) { window.setTimeout(step, 1000/60) };
 var keyDirection;
